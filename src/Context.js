ea26bc7409c1d68f0cf8aa09da6a8c41b78f1ac8
Marco-Medhat <eng.marcomedhat@gmail.com>
2019-01-25 20:08:47 +0200

    Pulling data from jasonplaceholder using axios

--- src/Context.js
+++ src/Context.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 const Context = React.createContext();
 
@@ -26,6 +27,12 @@ export class Provider extends Component {
 		contacts: [],
 		dispatch: action => this.setState(state => reducer(state, action))
 	};
+
+	componentDidMount() {
+		axios
+			.get("https://jsonplaceholder.typicode.com/users")
+			.then(res => this.setState({ contacts: res.data }));
+	}
 
 	render() {
 		return (
