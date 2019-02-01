cb699abb5d3d690c8d0bc2e2dab980252da59b50
Marco-Medhat <eng.marcomedhat@gmail.com>
2019-02-01 12:47:16 +0200

    Updates

---
+++ precache-manifest.63e7fdc7041115318c68d823817067d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8c4a8dcd6159d58da4f",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "c8c4a8dcd6159d58da4f",
+    "url": "/contactmanager/static/js/main.c8c4a8dc.chunk.js"
+  },
+  {
+    "revision": "27721b52a008d568f401",
+    "url": "/contactmanager/static/css/1.ca3a0560.chunk.css"
+  },
+  {
+    "revision": "27721b52a008d568f401",
+    "url": "/contactmanager/static/js/1.27721b52.chunk.js"
+  },
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "7d2862c68488884d89de1f1771d6e5d6",
+    "url": "/contactmanager/index.html"
+  }
+];
